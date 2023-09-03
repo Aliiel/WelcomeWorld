1d858d6bfe8692af00d2d5b593aaa4b0c68e0097
Aliiel <leila.mouaci@gmail.com>
2023-09-03 19:40:40 +0200

    mis en francais

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,7 +49,7 @@ class RegistrationController extends AbstractController
                 (new TemplatedEmail())
                     ->from(new Address('no-reply@www.com', 'mailwoo'))
                     ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
+                    ->subject('Veuillez confirmer votre adresse e-mail')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
@@ -77,7 +77,7 @@ class RegistrationController extends AbstractController
         }
 
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
-        $this->addFlash('success', 'Your email address has been verified.');
+        $this->addFlash('Nickel', 'Votre adresse e-mail a bien été vérifiée');
 
         return $this->redirectToRoute('app_register');
     }
--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -159,7 +159,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('no-reply@www.com', 'mailwoo'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Votre demande de nouveau mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
